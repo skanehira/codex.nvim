7e4f8ad7c824ddc27f66fb9755e328a8b896e64c
johnseth97 <17620345+johnseth97@users.noreply.github.com>
2025-04-18 19:32:45 -0400

    editing test init

--- tests/minimal_init.lua
+++ tests/minimal_init.lua
@@ -1,3 +1,4 @@
+pcall(require, "luacov.runner").init()
 vim.cmd("set rtp+=.")
 vim.cmd("set rtp+=./plenary.nvim") -- if using as a submodule or symlinked
 require("plugin.codex") -- triggers plugin/codex.lua
