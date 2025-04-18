b09381bc1207d36cdc23952d462103e3687af193
johnseth97 <17620345+johnseth97@users.noreply.github.com>
2025-04-18 18:03:57 -0400

    fixed plenary call

--- tests/run_cov.lua
+++ tests/run_cov.lua
@@ -1,3 +1,7 @@
+-- tests/run_cov.lua
+pcall(require, "luarocks.loader") -- add ~/.luarocks/**/5.1 to package.path
 require("luacov") -- start coverage
-require("plenary.test_harness").run() -- run all specs
+
+require("plenary.test_harness"):run() -- ← note the **colon**, not dot!
+
 vim.cmd("q")
